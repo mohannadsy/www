ccb25f8992e7b36f55312b0ce4afeb4bfbfe1d09
rgd <raghad.nanou@gmail.com>
2022-03-06 14:42:53 +0200

    hello

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -5,9 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/all.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/accountCard.css">
+
+    <link rel="stylesheet" href="{{asset('css/styles/accounts/accountCard.css')}}">
 </head>
 <body>
     <form action="">
--- resources/views/repository/index.blade.php
+++ resources/views/repository/index.blade.php
@@ -5,9 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/all.css">
-    <link rel="stylesheet" href="css/repository.css">
+    <link rel="stylesheet" href="{{asset('css/styles/repository/repository.css')}}">
 </head>
 <body>
     <form action=""> 
